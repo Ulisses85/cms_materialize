6c4a8529ddca09b3f77eed634b08cb7410ae95e5
Ulisses85 <lukasz.kuligowski@gmail.com>
2018-02-01 17:32:01 +0000

    login component logic and html , basic styling

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from './../../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+   loginFailed: boolean = false;
+   userRegistered: boolean = false;
+
+  constructor(
+    private router: Router,
+    private userService: UserService,
+
+
+  ) { }
 
   ngOnInit() {
+    if(localStorage.getItem("userRegistered")) {
+      this.userRegistered = true;
+      localStorage.removeItem("userRegistered");
+    }
   }
+
+  login({value, valid}) {
+    if (valid) {
+      this.userService.login(value).subscribe(res => {
+        if (res == 'invalidLogin') {
+          this.loginFailed = true;
+          setTimeout(function(){
+            this.loginFailed = false;
+          }.bind(this), 2000);
+        } else {
+          localStorage.setItem("user",JSON.stringify(res));
+          this.router.navigateByUrl('');
+        }
+      });
+    } else {
+      console.log('Form is not valid');
+    }
+  }
+
 
 }
