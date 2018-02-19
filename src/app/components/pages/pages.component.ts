26330c50f81f65f3c2dafbb15a6e81bbf7fda585
Ulisses85 <lukasz.kuligowski@gmail.com>
2018-02-19 15:37:49 +0000

    deleted nebula and material design icons for time being

--- src/app/components/pages/pages.component.ts
+++ src/app/components/pages/pages.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
+//import { NbSidebarModule, NbLayoutModule, NbSidebarService } from '@nebular/theme';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 
